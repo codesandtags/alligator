f1b44ec0504d09ab886dcf7b29d0f38c1465b910
Edwin Torres <codesandtags@gmail.com>
2018-02-03 14:08:48 -0500

    feat: Added more structure and modules in order to create the login

---
+++ src/app/modules/interview/interview.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InterviewRoutingModule } from './interview-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    InterviewRoutingModule
+  ],
+  declarations: []
+})
+export class InterviewModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
-  declarations: []
+  exports: [
+    MaterialModule,
+    FooterComponent
+  ],
+  declarations: [FooterComponent]
 })
-export class SharedModule { }
+export class SharedModule {
+}
