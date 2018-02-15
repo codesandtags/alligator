7b0346cabbb913c3241a6339036434322d387710
Edwin Torres <codesandtags@gmail.com>
2018-02-14 23:01:41 -0500

    Added data to deploy on Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '<your-key>',
-    authDomain: '<your-auth-domain>',
-    databaseURL: '<your-database-url>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage>',
-    messagingSenderId: '<your-message-id>'
+    apiKey: 'AIzaSyAaqOJGjS3lD1r7oXbZHR_ctXd61dDRkqk',
+    authDomain: 'alligator-2018.firebaseapp.com',
+    databaseURL: 'https://alligator-2018.firebaseio.com',
+    projectId: 'alligator-2018',
+    storageBucket: 'alligator-2018.appspot.com',
+    messagingSenderId: '562353340220'
   }
 };
